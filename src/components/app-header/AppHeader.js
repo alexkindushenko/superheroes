b43d46c3cd1fabdf0e8414cd51cbc352627cf57c
alexkindushenko <alikson22@gmail.com>
2022-08-09 13:04:48 +0300

    bugfix appHeder

--- src/components/app-header/AppHeader.js
+++ src/components/app-header/AppHeader.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import { setCurentPage } from "../../features/superheroes/superheroesSlice";
 
 import "./AppHeader.css";
 
 const AppHeader = () => {
+  const dispatch = useDispatch();
+
   return (
     <div className="app-header">
-      <div className="logo">
+      <div onClick={() => dispatch(setCurentPage(1))} className="logo">
         <NavLink to="superheroes">Superheroes</NavLink>
       </div>
 
       <nav className="menu">
         <ul>
-          <li>
+          <li onClick={() => dispatch(setCurentPage(1))}>
             <NavLink to="superheroes">Superheroes</NavLink>
           </li>
           <li>
